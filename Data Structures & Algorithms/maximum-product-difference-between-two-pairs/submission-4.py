bd0e3a29e9e96bc4b7fb7a73d2c3a374ab86ccab
bachnguyen-dataengineering <bachnguyen.bn.contact@gmail.com>
2026-06-19 21:26:26 -0500

    Add: maximum-product-difference-between-two-pairs - submission-4

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-4.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        print(nums)
+        return ((nums[n-1]*nums[n-2]) - (nums[1]*nums[0]))
